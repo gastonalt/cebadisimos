61f8633f9830b580a21cb67b5f2af1be72fe1433
Gaston Altamirano <gxsworld@gmail.com>
2025-09-10 23:35:48 -0300

    Agregamos cambio de escena cuando un personaje gana 5 veces, primera reunion de cebadisimos

--- GameManager.gd
+++ GameManager.gd
@@ -1,5 +1,7 @@
 extends Node
 
+var marcador = [0,0]
+
 signal jugador_muerto(player_id: int)
 
 # Called when the node enters the scene tree for the first time.
@@ -10,6 +12,11 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	var jugadores = get_tree().get_nodes_in_group("jugadores");
 	if jugadores and jugadores.size() < 2:
-		print("El jugador " + jugadores.get(0).name + " gana!")
-		get_tree().reload_current_scene()
+		print("El jugador " + jugadores.get(0).name + " gana la ronda")
+		marcador[jugadores.get(0).player_id - 1] = marcador[jugadores.get(0).player_id - 1] + 1
+		if marcador[0] == 5 or marcador[1] == 5:
+			print("El jugador " + jugadores.get(0).name + " gana la partida entera")
+			get_tree().change_scene_to_file("res://seleccionar_jugador_menu.tscn")
+		else:
+			get_tree().reload_current_scene()
 		pass
